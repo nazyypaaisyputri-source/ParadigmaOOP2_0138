f7f9e3fc52d64d075da8f6391c4b97a661f04d65
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-05-19 18:12:56 +0700

    isi data nama lampu pake fungsi setSaklarNo

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -16,5 +16,10 @@ string getSaklarNo(int i) {
 int main() {
     remoteLampu lampuRumah;
 
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
     return 0;
 } 
